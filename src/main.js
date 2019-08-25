a117e3c6ed866a18882b5f5a026c940c55e7d054
zelda <1930313663@qq.com>
2019-08-25 15:40:09 +0800

    element-ui导入

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 导入element-ui及样式
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// element-ui注册到vue实例上
+Vue.use(Element)
 Vue.config.productionTip = false
 
 new Vue({
